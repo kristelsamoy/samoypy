9c8e4a3f53537e302299c4af4de1f30ffe924b97
kristelsamoy <75332621+kristelsamoy@users.noreply.github.com>
2021-10-24 23:56:02 +0200

    .

--- oop/retta.py
+++ oop/retta.py
@@ -1,47 +1,41 @@
-class Retta:
+class retta:
+
+    def __init__(self,a,b,c):
     
-    NumeroRette = 0
-    
-    def __init__(self, coeff_X=int, coeff_Y=int, noto=int):
-        if coeff_X == 0 or coeff_Y == 0:
-            raise ZeroDivisionError
-        self.coeff_X = coeff_X
-        self.coeff_Y = coeff_Y
-        self.noto = noto
-        self.punti = []
-        Retta.NumeroRette += 1
+        self.__a = a  
+        self.__b = b
+        self.__c = c
+        
+    def equazione_imp(self):
+        if float(self.__c) == 0:
+            return f'{self.__a}x + {self.__b} = 0'
+        elif float(self.__b)==0:
+            return f'{self.__a}x + {self.__c} = 0'
+        elif  float(self.__a)== 0:
+            return f'{self.__b}y + {self.__c} = 0'
+        else:
+            return f'{self.__a}x +{self.__b}y + {self.__c} = 0'
 
-    def trova_y(self, x=int):
-        y = int(((self.coeff_X * x) + self.noto) / self.coeff_Y)
-        self.punti.append((x, y))
-        return y
+    def equazione_esp(self):
+        if int(self.__a) == 0:
+            return f'y = -{self.__c}/{self.__b}'
+        elif int(self.__c) == 0:
+            return f'y = -{self.__a}x/{self.__b}' 
+        elif int(self.__b) == 0:
+            return f'x = -{self.__c}/{self.__a}' 
+        else:
+            return f'y =(-{self.__a}/{self.__c}x + -{self.__c}/{self.__c}) '
+ 
+    def coefficiente(self):
+        if int(self.__b) == 0: 
+            return f'la retta è parallela all asse delle ordinate, quindi il coefficiente angolare è nullo' 
+        elif int(self.__a)== 0:
+            return f'la retta è parallela all asse delle ascisse, quindi il coefficiente angolare è uguale a 0 '
+        else:
+            return f'k = -{self.__a}/{self.__b} '
 
-    def trova_x(self, y=int):
-        x = int(((self.coeff_Y * y) - self.noto) / self.coeff_X)
-        self.punti.append((x, y))
-        return x
+a_b_c = retta (input("a: "), input("b: "), input("c: "))
 
-    def lista(self):
-        return self.punti
-
-    def coppie(self, start=1, stop=int, step=1):
-        return [(x, self.trova_y(x)) for x in range(start, stop, step)]
-
-    def __str__(self):
-        x = f"{self.coeff_X}X" if self.coeff_X != 1.0 else "X"
-        y = f"{self.coeff_Y}Y" if self.coeff_Y != 1.0 else "Y"
-        segno = "+" if self.noto > 0 else "-"
-        noto = f"{segno} {self.noto}" if self.noto != 0.0 else "" 
-        return f"{y} = {x} {noto}"
-
-    def __repr__(self):
-        return f"Retta({self.coeff_X}X, {self.coeff_Y}Y, {self.noto})"
-
-if __name__ == "__main__":
-    retta1 = Retta(1, 1, -4)
-    retta1.trova_x(5)
-    retta1.trova_y(8)
-    print(retta1)
-    print(retta1.lista())
-    print(repr(retta1))
-    print(retta1.coppie(8))
+print(a_b_c.equazione_imp())
+print(a_b_c.equazione_esp())
+print(a_b_c.coefficiente())
